20a417ad342cd23a157048d87fb02823679ac1b9
LJW <li_jin_wei_1995@163.com>
2017-06-27 12:48:13 +0800

    Initial commit

---
+++ ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FloatingButton
+//
+//  Created by Mac on 2017/6/27.
+//  Copyright © 2017年 lJW. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
